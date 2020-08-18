f3c3dca215bb9d4862c400e08068ea6a262aef11
Ryan Zhu <34224815+jorjiiie@users.noreply.github.com>
2020-08-18 14:23:31 -0400

    first test
    it's kind of broken but I think a bignum library should fix it

---
+++ miller_rabin.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <random>
+#include <chrono>
+// global variables bad but I don't want to keep creating random generators
+
+std::mt19937_64 rng(std::chrono::steady_clock::now().time_since_epoch().count());
+
+const int TESTS=50; // idk clrs says this is good so i use it lol
+
+long long fpow(long long a, long long b, long long m) {
+	long long ret = 1LL;
+	while(b) {
+		if (b&1) ret=a*a%m;
+		a=a*a%m;
+		b>>=1;
+	}
+	return ret%m;
+}
+bool test(long long a, long long n) {
+	// decompose n-1 into 2^t*u
+	long long t=0;
+	long long n2 = n-1;
+	while (!(n2&1)) t++,n2>>=1;
+	// n2=u now;
+	long long x;
+	long long last = fpow(a,n2,n);
+	for (int i = 0; i < t; i++) {
+		x=last*last%n;
+		if (x==1&&last!=1&&last!=n-1) {
+			return true;
+		}
+		last=x;
+	}
+	if (last!=1) return true;
+	return false;
+}
+bool miller_rabin(long long num, int tests) {
+	std::cout << num << "\n";
+	std::uniform_int_distribution<> dist(1,num-1);
+	for (int i = 0; i < tests; i++) {
+		long long cur = dist(rng);
+		std::cout << cur << "\n";
+		if (test(cur,num)) return false;
+	}
+	return true;
+}
+int main() {
+
+	std::cout << miller_rabin(1LL*2147483647,TESTS);
+}
